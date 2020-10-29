6aba8644f2ec4cc7a9ea2152a933a7c793cfa216
Yui <yunistar@icloud.com>
2020-10-29 14:31:37 +0100

    Eveything is explained in the README

--- ida64finder.py
+++ ida64finder.py
@@ -2,29 +2,21 @@ import idc
 import idaapi
 import ida_funcs
 
-# iBoot64Finder -f iBoot.bin -e >> ibot.txt
-
 def define_func(name, addr):
   ida_funcs.add_func(addr)
 
   print("[iBoot64Finder]: %s = %x" % (name, addr))
-  
+
   idc.set_name(addr, name, idc.SN_CHECK)
 
-# i64f("ibot.txt")
-
 def i64f(ibotlog):
-  function = " "
-
   with open(ibotlog, "r") as ibot:
-    function = ibot.readlines()
-
-    for line in function:
+    for line in ibot.readlines():
       if "=" in line:
         func = line.split()[1]
 
         if func == "base_addr": func = "_start"
 
         addr = int(line.split()[3], 0)
-        
+
         define_func(func, addr)
